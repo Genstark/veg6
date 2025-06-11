348f9af7b4aa97590efc9b1e9a9fe89327cabd63
Genstark <gautamy499@gmail.com>
2025-06-11 16:21:22 +0530

    new files

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-console.log('runing main.js');
-// console.log('runing main.js');
+const app = require('fastify')({ logger: true });
+const path = require('path');
+const fastifyStatic = require('@fastify/static');
+
+app.register(fastifyStatic, {
+    root: __dirname, // Serve static files from the current directory
+});
+
+app.get('/', async (request, reply) => {
+    reply.sendFile('index.html'); // Serve the index.html file
+});
+
+app.listen({ port: 3000 }, (err, address) => {
+    if (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
+    app.log.info(`Server listening at ${2000}`);
+});
